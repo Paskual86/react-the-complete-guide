9fa9b203770fa876f4bad0a2f92dbfbf5837db35
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-07-07 06:18:48 -0300

    Add modal behaviour

--- simple-app-with-dialog/src/components/UserInput/UserInput.jsx
+++ simple-app-with-dialog/src/components/UserInput/UserInput.jsx
@@ -7,17 +7,25 @@ import styles from "./UserInput.module.css";
 const UserInput = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
-
+  const [error, setError] = useState();
   const formSubmitHandler = (event) => {
     event.preventDefault();
 
     // Some validations
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
 
     // The plus (+) allow to convert the value to number
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please a valid age (greater than zero).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -33,9 +41,19 @@ const UserInput = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const onErrorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title= "an error occurs" message="error" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={onErrorHandler}
+        />
+      )}
       <Card className={styles.input}>
         <form onSubmit={formSubmitHandler}>
           <div>
