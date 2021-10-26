e6e6d458f35105a7c96c0e71402683aacd9adb47
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-10-26 07:09:07 -0300

    Adding Header component

---
+++ project-I-meal/src/components/Layout/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Aca vemos un ejemplo de como importar una imagen dentro de la pagina
+import imgMeal from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Mels</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={imgMeal} alt="some dummy description"></img>
+      </div>{" "}
+      {/* This will contain the image */}
+    </>
+  );
+};
+
+export default Header;
