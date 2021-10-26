31484ee2e77d9f9bda07583094ad761cd919ebbd
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2021-10-26 10:50:22 -0300

    Adding Button component

--- project-I-meal/src/components/Layout/Header.jsx
+++ project-I-meal/src/components/Layout/Header.jsx
@@ -3,12 +3,14 @@ import React from "react";
 // Aca vemos un ejemplo de como importar una imagen dentro de la pagina
 import imgMeal from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>React Mels</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
         <img src={imgMeal} alt="some dummy description"></img>
