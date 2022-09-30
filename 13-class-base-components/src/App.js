b14ba1a562140f6212fb200981b05ff2d36acf65
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2022-09-30 18:28:44 +0200

    Add Lecture 13-Class Based Component

---
+++ 13-class-base-components/src/App.js
@@ -0,0 +1,14 @@
+import Users from "./components/Users";
+/*This module I watch the video to understand the difference, but not apply the practice. I took some notes in base of the comments in the video.
+
+Something that it is interesting regarding this lecture is the point 171-Introducing Error Boundaries. This lecture is interested because teach how to catch 
+an exception in a global way. According to the teacher is not possible do using functional components. */
+function App() {
+  return (
+    <div>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
