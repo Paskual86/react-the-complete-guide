8a1cc77bc217908b24d0efdc3892e79774941daa
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2022-11-04 12:18:06 +0100

    Using a tag for links

---
+++ 20-building-multi-page/src/components/MainHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function MainHeader() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/welcome">Welcome</a>
+          </li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
