0ce3766e21339ab0d42e4c0d5de29dc54ce8c3a6
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2022-11-08 11:23:50 +0100

    Fetching Data With Parameters

---
+++ 20-react-router-6.4-intro-react-router-6.3/src/pages/Error.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+import MainNavigation from '../components/MainNavigation';
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  return (
+    <>
+      <MainNavigation />
+      <main id="error-content">
+        <h1>An Error Occurred!</h1>
+        <p>{error.message}</p>
+      </main>
+    </>
+  );
+}
