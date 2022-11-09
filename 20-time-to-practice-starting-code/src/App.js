88becaa0dd2786510b0b3645188eabf873d650a7
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2022-11-09 10:58:28 +0100

    Deploying React Apps

--- 20-time-to-practice-starting-code/src/App.js
+++ 20-time-to-practice-starting-code/src/App.js
@@ -1,29 +1,42 @@
+import React, { Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
+
 import Quotes from './pages/Quotes';
-import NotFound from './pages/NotFound';
+import LoadingSpinner from './components/UI/LoadingSpinner';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path="/quotes" exact>
-          <Quotes></Quotes>
-        </Route>
-        <Route path="/quotes/:quoteId">
-          <QuoteDetail></QuoteDetail>
-        </Route>
-        <Route path="/new-quote">
-          <NewQuote></NewQuote>
-        </Route>
-        <Route path="*">
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <Quotes></Quotes>
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail></QuoteDetail>
+          </Route>
+          <Route path="/new-quote">
+            <NewQuote></NewQuote>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
