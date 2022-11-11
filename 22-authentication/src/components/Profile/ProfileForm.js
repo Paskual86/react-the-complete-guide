265162e6b3e9867c0dc6464e5f0c2f9d1bb0ff65
Pablo Sebastian Andrada <pablosebastian.andrada@gmail.com>
2022-11-11 10:23:45 +0100

    Add change password

--- 22-authentication/src/components/Profile/ProfileForm.js
+++ 22-authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,36 @@
 import classes from './ProfileForm.module.css';
+import { useRef } from 'react';
+import { getAuth, updatePassword } from 'firebase/auth';
+
+import firebaseConfigApp from '../../firebaseConfig';
 
 const ProfileForm = () => {
+  const newPasswordRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const auth = getAuth(firebaseConfigApp);
+    console.log(auth.currentUser);
+    const newPassword = newPasswordRef.current.value;
+
+    updatePassword(auth.currentUser, newPassword)
+      .then(() => {})
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
